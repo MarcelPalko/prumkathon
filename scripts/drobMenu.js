4d8c8ec38c7db4d32937b52200ae7f9d9c76f5bb
lukasKotoun <lukas.kotoun107@seznam.cz>
2021-12-09 21:20:40 +0100

    add style dropmenu

--- scripts/drobMenu.js
+++ scripts/drobMenu.js
@@ -1,13 +1,14 @@
 const drobMenuDiv = document.getElementById("drob-menu")
-let pathSlice = window.location.pathname.split('/').slice(1, -1);
+path = window.location.pathname.split('/')
+let pathSlice = path.slice(1, -1);
 let currentPathName = "";
 pathSlice.forEach((element) => {
     currentPathName += element + "/";
     if (element !== "pages") {
         if (element === "obory") {
-            drobMenuDiv.innerHTML += `<a href =/#${element}>${element}</a>`
+            drobMenuDiv.innerHTML += `<a href =${path[0]}/#${element}>${element}</a>`
         } else {
-            drobMenuDiv.innerHTML += `<a href =${currentPathName}>${element}</a>`
+            drobMenuDiv.innerHTML += `<a href =/>${element}</a>`
         }
         if (element !== pathSlice[pathSlice.length - 1]) {
             drobMenuDiv.innerHTML += "<span> &gt </span>";
@@ -21,6 +22,3 @@ if (n.searchParams.get("subject")){
     drobMenuDiv.innerHTML += "<span> &gt </span>";
     drobMenuDiv.innerHTML += "<span>"+ param+"</span>"
 }
-
-
-
